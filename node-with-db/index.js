4442de05652fb15f8fd3460ec8964e82f4fc1f5b
Julian König <julian.koenig@js-soft.com>
2024-05-22 22:10:25 +0200

    chore: move stuff

--- node-with-db/index.js
+++ node-with-db/index.js
@@ -3,16 +3,11 @@
 import express from "express"
 import { MongoClient } from "mongodb"
 
-const app = express()
-
-app.use(express.json())
-
-const port = 4000
-
 const url = process.env.MONGODB_URL || "mongodb://localhost:27017"
-
 const client = await new MongoClient(url).connect()
 const collection = client.db("demo").collection("demo")
+
+const app = express().use(express.json())
 
 app.get("/", async (_, res) => {
   const found = await collection.find().toArray()
@@ -29,4 +24,5 @@ app.delete("/", async (_, res) => {
   res.status(200).json({ deleted: deleted.deletedCount })
 })
 
+const port = process.env.PORT || 4000
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
